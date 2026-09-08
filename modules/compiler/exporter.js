a47f56c4c65787df7b09e113270c0b0c27f82381
hsesite <hse.site@ptcgg.com>
2026-09-08 22:36:34 +0800

    Update exporter.js

--- modules/compiler/exporter.js
+++ modules/compiler/exporter.js
@@ -1,3 +1,36 @@
 // exporter.js
 
-console.log("Exporter siap.");
+const saveButton=document.createElement("button");
+
+saveButton.textContent="SIMPAN TEMPLATE";
+
+document.querySelector(".container").appendChild(saveButton);
+
+saveButton.onclick=function(){
+
+if(!compiledTemplate){
+
+alert("Compile form terlebih dahulu.");
+
+return;
+
+}
+
+const blob=new Blob(
+
+[JSON.stringify(compiledTemplate,null,2)],
+
+{type:"application/json"}
+
+);
+
+const a=document.createElement("a");
+
+a.href=URL.createObjectURL(blob);
+
+a.download=compiledTemplate.formId
+.replace(/[\/]/g,"_")+".json";
+
+a.click();
+
+};
