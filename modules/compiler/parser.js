7b4abc30d55c4fc9a67fd7d913628b66f1273d67
hsesite <hse.site@ptcgg.com>
2026-09-08 22:28:13 +0800

    Update parser.js

--- modules/compiler/parser.js
+++ modules/compiler/parser.js
@@ -1,1 +1,49 @@
+// parser.js
 
+const fileInput = document.getElementById("fileInput");
+const fileName = document.getElementById("filename");
+const compileBtn = document.getElementById("compileBtn");
+
+let selectedFile = null;
+
+fileInput.addEventListener("change", (e) => {
+  selectedFile = e.target.files[0];
+
+  if (selectedFile) {
+    fileName.textContent = selectedFile.name;
+  }
+});
+
+compileBtn.addEventListener("click", async () => {
+
+  if (!selectedFile) {
+    alert("Pilih file DOCX terlebih dahulu.");
+    return;
+  }
+
+  const ext = selectedFile.name.split(".").pop().toLowerCase();
+
+  if (ext !== "docx") {
+    renderPreview("Saat ini Compiler mendukung DOCX terlebih dahulu.\n\nPDF akan kita aktifkan pada tahap berikutnya.");
+    return;
+  }
+
+  try {
+
+    renderPreview("Membaca dokumen...");
+
+    const arrayBuffer = await selectedFile.arrayBuffer();
+
+    const result = await mammoth.extractRawText({
+      arrayBuffer
+    });
+
+    renderPreview(result.value);
+
+  } catch (err) {
+
+    renderPreview("Gagal membaca dokumen.\n\n" + err.message);
+
+  }
+
+});
