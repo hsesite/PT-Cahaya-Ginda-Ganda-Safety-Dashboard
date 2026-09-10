f7c7ab023602a3c0b95f7714e0e6992a0241164e
hsesite <hse.site@ptcgg.com>
2026-09-10 08:27:24 +0800

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,27 @@
+
+/* ==========================================================
+   PT CGG HSE Dashboard
+   UI Components
+========================================================== */
+
+const UI={
+
+placeholder(title){
+
+return `
+<div class="view-placeholder">
+
+<div class="placeholder-icon">
+<i data-lucide="sparkles"></i>
+</div>
+
+<h3>${title}</h3>
+
+<p>Modul sedang disiapkan.</p>
+
+</div>
+`;
+
+}
+
+};
