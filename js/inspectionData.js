2f9f0892059c39350d56357c3fe940cb07929b8c
hsesite <hse.site@ptcgg.com>
2026-09-11 00:10:46 +0800

    Create inspectionData.js

---
+++ js/inspectionData.js
@@ -0,0 +1,42 @@
+/* ==========================================================
+   inspectionData.js
+   Temporary Master Data
+========================================================== */
+
+const InspectionData={
+
+  company:[
+    "PT Cahaya Ginda Ganda"
+  ],
+
+  contractor:[
+    "VIP",
+    "SLS"
+  ],
+
+  subContractor:{
+
+    VIP:["CHN","BCJ","PAR","ARL"],
+
+    SLS:[]
+
+  },
+
+  category:[
+
+    "Unsafe Action",
+    "Unsafe Condition",
+    "Good Practice"
+
+  ],
+
+  risk:[
+
+    "Rendah",
+    "Sedang",
+    "Tinggi",
+    "Kritis"
+
+  ]
+
+};
